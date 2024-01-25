ac0afa306faee9d3b7b0c9912dec5616e5a49a8b
Keitaro Oguri <ogukei256@gmail.com>
2024-01-26 02:04:17 +0900

    Modify

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "openstack_compute_keypair_v2" "keypair" {
 # Boot Volume
 # https://doc.conoha.jp/api-vps3/volume-create_vol-v3/
 resource "openstack_blockstorage_volume_v3" "volume" {
-  name = "myvol"
+  name = var.volume_name
   size = 100
   image_id = var.image_id
   volume_type = var.volume_type_id
@@ -34,7 +34,7 @@ resource "openstack_compute_instance_v2" "instance" {
   name = var.instance_name
   flavor_id = var.flavor_id
   key_pair = openstack_compute_keypair_v2.keypair.id
-  security_groups = [openstack_networking_secgroup_v2.secgroup.id]
+  security_groups = [openstack_networking_secgroup_v2.secgroup.name]
   admin_pass = var.instance_admin_pass
   metadata = {
     instance_name_tag = var.instance_name_tag
@@ -60,6 +60,5 @@ resource "openstack_networking_secgroup_rule_v2" "secgroup_rule" {
   protocol = "udp"
   port_range_min = 8211
   port_range_max = 8211
-  remote_ip_prefix = "0.0.0.0/0"
   security_group_id = openstack_networking_secgroup_v2.secgroup.id
 }
